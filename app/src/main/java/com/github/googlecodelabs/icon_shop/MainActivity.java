2146a44842fbc773672e704effceaa1bb84b0088
birdinc <djefferys926@gmail.com>
2016-12-24 12:55:32 -0500

    changes to MainActivity

--- app/src/main/java/com/github/googlecodelabs/icon_shop/MainActivity.java
+++ app/src/main/java/com/github/googlecodelabs/icon_shop/MainActivity.java
@@ -1,17 +1,3 @@
-// Copyright 2016 Google Inc.
-// 
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-// 
-//      http://www.apache.org/licenses/LICENSE-2.0
-// 
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package com.github.googlecodelabs.icon_shop;
 
 import android.content.Intent;
@@ -35,6 +21,8 @@ import android.view.View;
 import android.widget.Button;
 
 import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.firebase.analytics.FirebaseAnalytics.Event;
+import com.google.firebase.analytics.FirebaseAnalytics.Param;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -130,6 +118,10 @@ public class MainActivity extends AppCompatActivity
 
     private void onProfessionSelected(MenuItem item) {
         final String profession = item.getTitle().toString();
+        // Added from Code Lab
+        FirebaseAnalytics analytics = FirebaseAnalytics.getInstance( this );
+        analytics.setUserProperty( "profession", profession );
+
         AsyncTask<Void, Void, Void> save = new AsyncTask<Void, Void, Void>() {
             @Override
             protected Void doInBackground(Void... voids) {
