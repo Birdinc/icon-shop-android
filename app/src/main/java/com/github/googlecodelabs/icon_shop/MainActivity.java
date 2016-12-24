54199ccc445753d546a28fe6b5e87e1a557ef026
birdinc <djefferys926@gmail.com>
2016-12-23 21:03:32 -0500

    added Firebase API key. successful build.

--- app/src/main/java/com/github/googlecodelabs/icon_shop/MainActivity.java
+++ app/src/main/java/com/github/googlecodelabs/icon_shop/MainActivity.java
@@ -34,11 +34,15 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.analytics.FirebaseAnalytics;
+
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     private RecyclerView.LayoutManager mItemsLayoutManager;
     private RecyclerView.Adapter mAdapter;
+    private FirebaseAnalytics mFirebaseAnalytics;
+
     private Button mSelectProfessionButton;
 
     private RecyclerView getItemsRecycleView() {
@@ -68,6 +72,8 @@ public class MainActivity extends AppCompatActivity
 
         mSelectProfessionButton = (Button) findViewById(R.id.select_profession_button);
 
+        // Obtain the FirebaseAnalytics instance.
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
 
         Toolbar toolbar = getToolbar();
         setSupportActionBar(toolbar);
